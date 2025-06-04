f3c17826ba466fa6667e263267e234e119e30e01
Aparnakukkadapu <aparnakukkadapu@gmail.com>
2025-06-04 17:53:38 +0530

    ASCII value program

---
+++ ASCII_value.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+               
+       char ch='T';
+       int asciivalue = (int) ch;
+
+       System.out.println("ASCII value of this character is " +asciivalue);
+    }
+}
