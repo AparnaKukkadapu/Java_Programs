a55fddc11ba508ceeee5d20982b939970edf7c42
Aparnakukkadapu <aparnakukkadapu@gmail.com>
2025-06-05 10:26:04 +0530

    adding even_odd_numbers program

---
+++ Even_odd_numbers.java
@@ -0,0 +1,16 @@
+class Even_odd_numbers {
+    public static void main(String[] args) {
+               
+        int num=20;
+        
+        if(num%2==0)
+        {
+            System.out.println("Number is even");
+        }
+        else
+        {
+            System.out.println("number is odd");
+        }
+        
+    }
+}
