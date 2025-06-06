8515c8d59fb41dfd24de97a5593ce5e2e36961bd
Aparnakukkadapu <aparnakukkadapu@gmail.com>
2025-06-06 12:49:10 +0530

    program to check whether a character is a alphabet or not

---
+++ alphabet_character.java
@@ -0,0 +1,18 @@
+//this program is to check the leap year 
+
+public class alphabet_character {    
+    
+    public static void main(String[] args) {
+     
+        char ch='T';
+
+        if(ch>='a' && ch<='z'||ch>='A' && ch<='Z')
+        {
+            System.out.println(ch +" character is a alphabet");
+        }
+        else
+         {
+            System.out.println(ch +" character is not a alphabet");
+        }
+    }
+}
