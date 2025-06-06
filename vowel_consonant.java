a4656a2e45c0955d05e6ca3da7f10241743a09e4
Aparnakukkadapu <aparnakukkadapu@gmail.com>
2025-06-06 12:27:44 +0530

    vowel and consonant java program

---
+++ vowel_consonant.java
@@ -0,0 +1,18 @@
+//this program is to check the leap year 
+
+public class vowel_consonant {    
+    
+    public static void main(String[] args) {
+               
+        char ch='U';
+               
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'|ch=='O'||ch=='U')
+        {
+            System.out.println(ch +" character is vowel");
+        }
+        else
+        {
+            System.out.println(ch +" character is a consonant");
+        }
+    }
+}
